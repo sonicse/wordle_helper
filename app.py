268e7274ba63354ef0700007d7983f76a12276e7
sonicse <sonicse@gmail.com>
2026-01-03 19:40:34 +0500

    add clear button

--- app.py
+++ app.py
@@ -6,19 +6,33 @@ import wordle
 st.title("Wordle Helper")
 col1, col2, col3, col4, col5 = st.columns(5)
 
-char_1 = col1.text_input(label="1", max_chars=1)
-char_2 = col2.text_input(label="2", max_chars=1)
-char_3 = col3.text_input(label="3", max_chars=1)
-char_4 = col4.text_input(label="4", max_chars=1)
-char_5 = col5.text_input(label="5", max_chars=1)
+char_1 = col1.text_input(label="1", max_chars=1, key="char_1")
+char_2 = col2.text_input(label="2", max_chars=1, key="char_2")
+char_3 = col3.text_input(label="3", max_chars=1, key="char_3")
+char_4 = col4.text_input(label="4", max_chars=1, key="char_4")
+char_5 = col5.text_input(label="5", max_chars=1, key="char_5")
 
 chars = [char_1, char_2, char_3, char_4, char_5]
 
-exclude = st.text_input("Не совпадающие символы:", placeholder="абвгде..")
-include = st.text_input("Совпадающие символы:", placeholder="жзиклмн..")
+exclude = st.text_input("Не совпадающие символы:", placeholder="абвгде..", key="exclude")
+include = st.text_input("Совпадающие символы:", placeholder="жзиклмн..", key="include")
 
-if st.button("Варианты"):
-    words = wordle.words_gen("C:/Users/AAnikeev/PycharmProjects/wordle_helper/data/russian_nouns.txt")
+def clear():
+    st.session_state["char_1"] = ""
+    st.session_state["char_2"] = ""
+    st.session_state["char_3"] = ""
+    st.session_state["char_4"] = ""
+    st.session_state["char_5"] = ""
+    st.session_state["include"] = ""
+    st.session_state["exclude"] = ""
+
+but1, but2 = st.columns(2)
+
+def words():
+    words = wordle.words_gen("C:/Users/sonicse/.work/github/wordle_helper/data/russian_nouns.txt")
     words = wordle.filter_words(words, exclude=exclude, include=include, chars=chars)
     words_text = "\n".join(words)
-    st.text(words_text)
+    but1.text(words_text)
+
+but1.button("Варианты", on_click=words)
+but2.button("Очистить", on_click=clear)
